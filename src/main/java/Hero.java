6d0abb5a0f4f0657c3c1f85a34513d36343f42ac
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-14 12:48:21 +0300

    test to instantiate correctly passed

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Hero {
+    private String mName;
+
+
+    public Hero(String name){
+        mName=name;
+    }
+    public String getName(){
+        return mName;
+    }
+
 }
+
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,2 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class HeroTest {
+    @Test
+    public void Hero_instantiatesCorrectly_true(){
+        Hero testHero = new Hero("Superman");
+                assertEquals(true,testHero instanceof Hero);
+    }
 }
