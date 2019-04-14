1a5661bf4da8d998f4a1e647f0f5940476873457
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-14 23:55:41 +0300

    test to get size done

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,12 +3,17 @@
 
 public class Squad {
     private String mName;
+    private int mSize;
 
-    public Squad(String name){
+    public Squad(String name, int size){
         mName = name;
+        mSize = size;
     }
     public String getName(){
         return mName;
+    }
+    public int getSize() {
+        return mSize;
     }
 
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -5,13 +5,18 @@ import static org.junit.Assert.*;
 public class SquadTest {
     @Test
     public void Squad_instantiatesCorrectly_true(){
-        Squad testSquad = new Squad(" Sevens Squad");
+        Squad testSquad = new Squad(" Sevens Squad",4);
         assertEquals(true, testSquad instanceof Squad);
     }
     @Test
     public void getName_instantiatesWithName_String(){
-        Squad testSquad = new Squad("Sevens Squad");
+        Squad testSquad = new Squad("Sevens Squad",4);
         assertEquals("Sevens Squad",testSquad.getName());
+    }
+    @Test
+    public void getSize_squadInstantiatesWithSize_int() {
+        Squad testSquad = new Squad("Sevens Squad",4);
+        assertEquals(4, testSquad.getSize());
     }
 
 }
