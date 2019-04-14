a06ca62580181ee893377a3a821e6ea8cf07350c
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-14 13:17:19 +0300

    spelling errors corrected

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,33 +4,33 @@ import static org.junit.Assert.*;
 public class HeroTest {
     @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero testHero = new Hero("Superman",21,"Flying","Kryptonite");
+        Hero testHero = new Hero("Batman",30,"Strength","Parents");
                 assertEquals(true,testHero instanceof Hero);
 
     }
 
     @Test
     public void getName_instantiatesWithName_String() {
-        Hero testHero = new Hero("Superman",21,"Flying","Kryptonite");
-        assertEquals("Superman", testHero.getName());
+        Hero testHero = new Hero("Batman",30,"Strength","Parents");
+        assertEquals("Batman", testHero.getName());
     }
 
     @Test
     public void getAge_instantiatesWithAge_int() {
-        Hero testHero = new Hero("Superman",21,"Flying","Kryptonite");
-        assertEquals(21, testHero.getAge());
+        Hero testHero = new Hero("Batman",30,"Strength","Parents");
+        assertEquals(30, testHero.getAge());
     }
 
     @Test
     public void getPower_instantiatesWithPower_String() {
-        Hero testHero = new Hero("Superman",21,"Flying","Kryptonite");
-        assertEquals("Flying", testHero.getPower());
+        Hero testHero = new Hero("Batman",30,"Strength","Parents");
+        assertEquals("Strength", testHero.getPower());
     }
 
     @Test
     public void getWeakness_instantiatesWithWeakness_String() {
-        Hero testHero = new Hero("Superman",21,"Flying","Kryptonite");
-        assertEquals("Kryptonite", testHero.getWeakness());
+        Hero testHero = new Hero("Batman",30,"Strength","Parents");
+        assertEquals("Parents", testHero.getWeakness());
     }
 
 
