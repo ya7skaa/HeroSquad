9f49f48581a54c4792fd7d9bcee527cffdbb588d
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-15 01:22:58 +0300

    created layout file

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -8,7 +8,7 @@ public class Hero {
     private String mPower;
     private String mWeakness;
     private static List<Hero> instances = new ArrayList<Hero>();
-//    private int mId;
+    private int mId;
 
 
     public Hero(String name,int age,String power,String weakness){
@@ -17,27 +17,53 @@ public class Hero {
         mPower = power;
         mWeakness = weakness;
         instances.add(this);
-//        mId = instances.size();
+        mId = instances.size();
     }
+
+    //method for getting hero name
     public String getName(){
         return mName;
     }
+
+    // method for getting hero age
     public   int getAge(){
         return mAge;
     }
+
+    // method for getting hero power
     public String getPower(){
         return mPower;
     }
+
+
+    //Method for getting hero Weakness
     public String getWeakness(){
         return mWeakness;
     }
+
+    //Method to return all heros in the list
     public static List<Hero> all() {
         return instances;
     }
 
+    //Method to clear heros from ArrayList
+
     public static void clear() {
         instances.clear();
     }
+
+    // method  for instantiating with Id
+    public int getId() {
+        return mId;
+    }
+
+    // method to find a specific ID
+    public static Hero find(int id){
+
+        return instances.get(id - 1);
+    }
+
+
 
 
 
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,7 +1,10 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+//import java.util.List;
 
 public class HeroTest {
+
+    //Test to check if it will instantiate hero correctly
     @Test
     public void Hero_instantiatesCorrectly_true(){
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
@@ -9,29 +12,35 @@ public class HeroTest {
 
     }
 
+    //Test to check if Hero can be assigned a Name
     @Test
     public void getName_instantiatesWithName_String() {
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
         assertEquals("Batman", testHero.getName());
     }
 
+    //Test to check if Hero can be assigned an Age
     @Test
     public void getAge_instantiatesWithAge_int() {
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
         assertEquals(30, testHero.getAge());
     }
 
+    //Test to check if Hero can be assigned a Power
     @Test
     public void getPower_instantiatesWithPower_String() {
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
         assertEquals("Strength", testHero.getPower());
     }
 
+    //Test to check if Hero can be assigned a Weakness
     @Test
     public void getWeakness_instantiatesWithWeakness_String() {
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
         assertEquals("Parents", testHero.getWeakness());
     }
+
+    //Test to return all instances of of Squad
     @Test
     public void all_returnsAllInstancesOfHero_true() {
         Hero firstHero = new Hero("Batman",30,"Strength","Parents");
@@ -39,12 +48,39 @@ public class HeroTest {
         assertEquals(true, Hero.all().contains(firstHero));
         assertEquals(true, Hero.all().contains(secondHero));
     }
+
+    //Test to clear the list of all Squads
     @Test
     public void clear_emptiesAllHeroesFromArrayList_0() {
         Hero testHero = new Hero("Batman",30,"Strength","Parents");
         Hero.clear();
         assertEquals(Hero.all().size(), 0);
     }
+
+    //Test of Assigning Unique Id
+    @Test
+    public void getId_SquadsInstantiateWithId_1(){
+
+        Hero.clear(); //To clear List for test to work
+
+        Hero myHero = new Hero("Batman",30,"Strength","Parents");
+
+        assertEquals(1, myHero.getId());
+
+    }
+    //Test for finding Specific object
+//    @Test
+//    public void squad_WillReturnSquadWithSameId_secondSquad(){
+//
+//        Hero firstHero = new Hero("Batman",30,"Strength","Parents");
+//
+//        Hero secondHero = new Hero("Spiderman",17,"Flying","Age");
+//
+//        assertEquals(Hero.find(firstHero.getId()), secondHero);
+//
+//    }
+
+
 
 
 
