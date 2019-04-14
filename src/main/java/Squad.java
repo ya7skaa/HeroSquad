0308defde191d7131bb8789ee2355015a5a3c76a
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-15 00:01:28 +0300

    test for the squad cause passed

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -4,16 +4,21 @@
 public class Squad {
     private String mName;
     private int mSize;
+    private String mCause;
 
-    public Squad(String name, int size){
+    public Squad(String name, int size,String cause){
         mName = name;
         mSize = size;
+        mCause = cause;
     }
     public String getName(){
         return mName;
     }
     public int getSize() {
         return mSize;
+    }
+    public String getCause(){
+        return mCause;
     }
 
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -5,18 +5,23 @@ import static org.junit.Assert.*;
 public class SquadTest {
     @Test
     public void Squad_instantiatesCorrectly_true(){
-        Squad testSquad = new Squad(" Sevens Squad",4);
+        Squad testSquad = new Squad(" Sevens Squad",4,"Fighting bad breath");
         assertEquals(true, testSquad instanceof Squad);
     }
     @Test
     public void getName_instantiatesWithName_String(){
-        Squad testSquad = new Squad("Sevens Squad",4);
+        Squad testSquad = new Squad("Sevens Squad",4,"Fighting bad breath");
         assertEquals("Sevens Squad",testSquad.getName());
     }
     @Test
-    public void getSize_squadInstantiatesWithSize_int() {
-        Squad testSquad = new Squad("Sevens Squad",4);
+    public void getSize_instantiatesWithSize_int() {
+        Squad testSquad = new Squad("Sevens Squad",4,"Fighting bad breath");
         assertEquals(4, testSquad.getSize());
+    }
+    @Test
+    public void getCause_instantiatesWithCause_String(){
+        Squad testSquad = new Squad("Sevens Squad",4,"Fighting bad breath");
+        assertEquals("Fighting bad breath",testSquad.getCause());
     }
 
 }
