e8251f3a4e7989042e6287b01bcfdf7520b28fad
Abdirahman Osman <abdirahmanosman@Abdirahmans-MBP.lan>
2019-04-15 21:18:48 +0300

    heading1 styled to centre

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -168,14 +168,14 @@ public class App {
 
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-        get("/squads/:id", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
-            model.put("squad", squad);
-            model.put("heroes", hero.getHeroes());
-            model.put("template", "templates/squad.vtl");
-            return new ModelAndView(model, layout);
-        }, new VelocityTemplateEngine());
+//        get("/squads/:id", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+//            model.put("squad", squad);
+//            model.put("heroes", hero.getHeroes());
+//            model.put("template", "templates/squad.vtl");
+//            return new ModelAndView(model, layout);
+//        }, new VelocityTemplateEngine());
 
 
 
