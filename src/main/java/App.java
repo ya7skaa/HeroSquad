871c7a84819615a93510dd12cc95ed69e1628e4c
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-16 15:25:29 +0300

    changed font fot all the headings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -130,6 +130,8 @@ public class App {
             String weakness = request.queryParams("heroWeakness");
             int age = Integer.parseInt(request.queryParams("heroAge"));
             Hero myHero = new Hero(name, age, power, weakness);
+            Squad squad = Squad.find(Integer.parseInt(request.queryParams("squadId")));
+            squad.addHero(myHero);
             model.put("template", "templates/herosuccess.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
@@ -164,16 +166,25 @@ public class App {
             squad.addHero(myHero);
 
             model.put("squad", squad);
-            model.put("template", "templates/heros.success.vtl");
+            model.put("template", "templates/herosuccess.vtl");
 
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
 //        get("/squads/:id", (request, response) -> {
 //            Map<String, Object> model = new HashMap<String, Object>();
 //            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
 //            model.put("squad", squad);
-//            model.put("heroes", hero.getHeroes());
+////            model.put("heroes", hero.getHeroes());
 //            model.put("template", "templates/squad.vtl");
+//            return new ModelAndView(model, layout);
+//        }, new VelocityTemplateEngine());
+
+//        get("/heroes/:id", (request, response) -> {
+//            HashMap<String, Object> model = new HashMap<String, Object>();
+//            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+//            model.put("hero", hero);
+//            model.put("template", "templates/hero.vtl");
 //            return new ModelAndView(model, layout);
 //        }, new VelocityTemplateEngine());
 
